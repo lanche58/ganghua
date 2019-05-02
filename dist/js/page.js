00fbda3633397c5716f6a1ec66ebfa9df9ecdfa3
renrui <675603402@qq.com>
2019-05-03 03:03:22 +0800

    第四次提交

--- dist/js/page.js
+++ dist/js/page.js
@@ -39,24 +39,33 @@ function pageBox() {
         isMobile = false;
     };
 
-    if(w_width <= 861){
-        var danNum=0;
-        $('.pnav ul').css('display','none');
-        $('.pnav').bind(_click,function(){
-            if(danNum==1){
-                $(this).removeClass('show');
-                $(this).find('ul').stop().fadeOut();
-                danNum = 0;
-            }else{
-                $(this).addClass('show');
-                $(this).find('ul').stop().fadeIn();
-                danNum = 1;
+    // if(w_width <= 861){
+    //     var danNum=0;
+    //     $('.pnav ul').css('display','none');
+    //     $('.pnav').bind(_click,function(){
+    //         if(danNum==1){
+    //             $(this).removeClass('show');
+    //             $(this).find('ul').stop().fadeOut();
+    //             danNum = 0;
+    //         }else{
+    //             $(this).addClass('show');
+    //             $(this).find('ul').stop().fadeIn();
+    //             danNum = 1;
+    //         }
+    //     });
+    // }else{
+    //     $('.pnav').removeClass('show');
+    //     $('.pnav ul').css('display','block');
+    //     $('.pnav').unbind(_click);
+    // }
+
+    if (isMobile) {
+        $('.pnav li').each(function() {
+            var _act = $(this).find('a').hasClass('act');
+            if (_act) {
+                $('.pnav').scrollLeft($(this).position().left);
             }
         });
-    }else{
-        $('.pnav').removeClass('show');
-        $('.pnav ul').css('display','block');
-        $('.pnav').unbind(_click);
     }
 };
 
@@ -67,6 +76,11 @@ $(window).resize(function () {
 });
 
 $(function () {
+    if ($('.splwo').length !== 0) {
+        $('.splwo').each(function(i) {
+            splitWords($(this));
+        });
+    }
 	
 	
 
@@ -308,7 +322,10 @@ function splitWords(el) {
         for (var j = 0; j < _test[i].length; j++) {
             _span.eq(i).append('<i></i>');
             var _i = _span.eq(i).find('i');
-            _i.eq(j).text(_test[i][j]).css({
+            if (_test[i][j] === " ") {
+                _test[i][j] = "&nbsp;";
+            }
+            _i.eq(j).html(_test[i][j]).css({
                 'animation-delay': 100*j + 'ms',
                 '-webkit-animation-delay': 100*j + 'ms'
             });
